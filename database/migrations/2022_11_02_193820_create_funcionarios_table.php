ca285ad5597d2b88ea5b28e8574636c21057d426
juan rosas <juan.rosas.andrade@gmail.com>
2022-11-02 17:59:45 -0300

    primer commit

---
+++ database/migrations/2022_11_02_193820_create_funcionarios_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 45);
+            $table->unsignedBigInteger('grado_id')->nullable();
+
+            $table->foreign('grado_id')
+                ->reference('id')
+                ->on('grados')
+                ->onDelete('set null');
+
+            $table->string('apellido_paterno', 45);
+            $table->string('apellido_materno', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+};
